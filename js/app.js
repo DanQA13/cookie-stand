85d384e0e364904b3c26a29f9a2661c495c05228
Daniel Cornier <D.cornier1386@outlook.com>
2021-10-17 21:30:59 -0400

    done

--- js/app.js
+++ js/app.js
@@ -1,47 +1,196 @@
 
-"use strict";
 
-const seattleStore = document.getElementById('seattleList'); 
-let hours = ['6 a.m', '7 a.m.', '8 a.m.', '9a.m.','10 a.m','11 a.m','12 p.m','1 p.m','2 p.m','3 p.m','4 p.m','5 p.m','6 p.m','7 p.m'];
-let seattle = {
+let seattleList = document.getElementById('seattlecookies');
+let tokyoList = document.getElementById('tokyocookies');
+let dubaiList = document.getElementById('dubaicookies');
+let parisList = document.getElementById('pariscookies');
+let limaList = document.getElementById('limacookies');
+let hours = [
+  '6 AM',
+  '7 AM',
+  '8 AM',
+  '9 AM',
+  '10 AM',
+  '11 AM',
+  '12 PM',
+  '1 PM',
+  '2 PM',
+  '3 PM',
+  '4 PM',
+  '5 PM',
+  '6 PM',
+  '7 PM',
+];
 
-	name: 'Seattle',
-	min: 23,
-	max: 65,
-	avg: 6.3,
-	cookiesSoldEachHourArray: [],
-	// a number between min and max that times by the average, maybe a method?
-	getRandomCustomers: function() {
-	
-	return Math.floor(Math.random() * (this.max - this.min +1) + this.min);
-	
-	},
-	getCookieSalesPerHour: function() {
-    //calculate the cookies sold for all 14 hours
+let seattleLocation = {
+  name: 'Seattle',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+  cookiesSoldEachHourArray: [],
 
+  getRandomCustomers: function() {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  getCookieSalesPerHour : function() {
+    let cookiesSoldEachHour = Math.ceil(this.getRandomCustomers() * this.avg);
+    return cookiesSoldEachHour;
 
-    //get the number of customers this hour
-  
+  },
 
-  
-  
-  //that value of total cookies sold that hour onto our array
-    // how to get the value of the total cookies sold this hour?
-    // write a for loop that uses the length of the hours array in that loop
-
-    for (let i =0; i < hours.length; i++) {
-    let customersThisHour = this.getRandomCustomers(); 
-    let totalCookiesSoldThisHour = Math.ceil(customersThisHour * this.avg);
-    // this.totalCookiesSoldThisHour;
-    this.cookiesSoldEachHourArray.push(totalCookiesSoldThisHour);
+  renderList: function() {
     
+    for(let i = 0; i < hours.length; i++) {
+      let cookiesSoldEachHour = Math.ceil(this.getRandomCustomers() * this.avg);
+      this.cookiesSoldEachHourArray.push(this.getRandomCustomers());
+      let li = document.createElement('li');
+      li.textContent = `${hours[i]}: ${this.getCookieSalesPerHour()} cookies`;
+      seattleList.appendChild(li);
+
     }
-    console.log(this.cookiesSoldEachHourArray);
-	},
-  renderTheList: function (){
-    this.getCookieSalesPerHour();
-    //code that will render the lists to the page
-    //render the lis inside our ul
+  
+
   }
-}
-seattle.renderTheList();
+
+
+
+};
+
+let tokyoLocation = {
+  name: 'Tokyo',
+  min: 3,
+  max: 24,
+  avg: 1.2,
+  cookiesSoldEachHourArray: [],
+
+  getRandomCustomers: function() {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+
+  getCookieSalesPerHour : function() {
+    let cookiesSoldEachHour = Math.ceil(this.getRandomCustomers() * this.avg);
+    return cookiesSoldEachHour;
+
+  },
+
+  renderList: function() {
+    for(let i = 0; i < hours.length; i++) {
+      let cookiesSoldEachHour = Math.ceil(this.getRandomCustomers() * this.avg);
+      this.cookiesSoldEachHourArray.push(this.getRandomCustomers());
+      let li = document.createElement('li');
+      li.textContent = `${hours[i]}: ${this.getCookieSalesPerHour()} cookies`;
+      tokyoList.appendChild(li);
+
+    }
+ 
+
+  }
+
+
+
+};
+
+let dubaiLocation = {
+  name: 'Dubai',
+  min: 11,
+  max: 38,
+  avg: 3.7,
+  cookiesSoldEachHourArray: [],
+
+  getRandomCustomers: function() {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+
+  getCookieSalesPerHour : function() {
+    let cookiesSoldEachHour = Math.ceil(this.getRandomCustomers() * this.avg);
+    return cookiesSoldEachHour;
+
+  },
+
+  renderList: function() {
+    for(let i = 0; i < hours.length; i++) {
+      let cookiesSoldEachHour = Math.ceil(this.getRandomCustomers() * this.avg);
+      this.cookiesSoldEachHourArray.push(this.getRandomCustomers());
+      let li = document.createElement('li');
+      li.textContent = `${hours[i]}: ${this.getCookieSalesPerHour()} cookies`;
+      dubaiList.appendChild(li);
+
+    }}
+
+
+
+};
+
+let parisLocation = {
+  name: 'Paris',
+  min: 20,
+  max: 38,
+  avg: 2.3,
+  cookiesSoldEachHourArray: [],
+
+  getRandomCustomers: function() {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+
+  getCookieSalesPerHour : function() {
+    let cookiesSoldEachHour = Math.ceil(this.getRandomCustomers() * this.avg);
+    return cookiesSoldEachHour;
+
+  },
+
+  renderList: function() {
+    for(let i = 0; i < hours.length; i++) {
+      let cookiesSoldEachHour = Math.ceil(this.getRandomCustomers() * this.avg);
+      this.cookiesSoldEachHourArray.push(this.getRandomCustomers());
+      let li = document.createElement('li');
+      li.textContent = `${hours[i]}: ${this.getCookieSalesPerHour()} cookies`;
+      parisList.appendChild(li);
+
+    }
+ 
+
+  }
+
+
+
+};
+
+let limaLocation = {
+  name: 'Paris',
+  min: 2,
+  max: 16,
+  avg: 4.6,
+  cookiesSoldEachHourArray: [],
+
+  getRandomCustomers: function() {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+
+  getCookieSalesPerHour : function() {
+    let cookiesSoldEachHour = Math.ceil(this.getRandomCustomers() * this.avg);
+    return cookiesSoldEachHour;
+
+  },
+
+  renderList: function() {
+    for(let i = 0; i < hours.length; i++) {
+      let cookiesSoldEachHour = Math.ceil(this.getRandomCustomers() * this.avg);
+      this.cookiesSoldEachHourArray.push(this.getRandomCustomers());
+      let li = document.createElement('li');
+      li.textContent = `${hours[i]}: ${this.getCookieSalesPerHour()} cookies`;
+      limaList.appendChild(li);
+
+    }
+  }
+
+
+
+};
+
+
+
+seattleLocation.renderList();
+tokyoLocation.renderList();
+dubaiLocation.renderList();
+parisLocation.renderList();
+limaLocation.renderList(); 
